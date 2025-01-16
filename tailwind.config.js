894269195fbece523a96ef11b9f8fecd2c95803a
saimoulibandari <saimouli.bandari@gmail.com>
2025-01-17 03:08:48 +0530

    updated and added new files and modified existing files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,76 +1,90 @@
-const { hairlineWidth } = require('nativewind/theme');
+const { hairlineWidth } = require("nativewind/theme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  presets: [require('nativewind/preset')],
+  darkMode: "class",
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
-        border: 'hsla(var(--border))',
-        input: 'hsla(var(--input))',
-        ring: 'hsla(var(--ring))',
-        background: 'hsla(var(--background))',
-        foreground: 'hsla(var(--foreground))',
+        border: "hsla(var(--border))",
+        input: "hsla(var(--input))",
+        ring: "hsla(var(--ring))",
+        background: "hsla(var(--background))",
+        foreground: "hsla(var(--foreground))",
         primary: {
-          DEFAULT: 'hsla(var(--primary))',
-          foreground: 'hsla(var(--primary-foreground))',
+          DEFAULT: "hsla(var(--primary))",
+          foreground: "hsla(var(--primary-foreground))",
         },
         secondary: {
-          DEFAULT: 'hsla(var(--secondary))',
-          foreground: 'hsla(var(--secondary-foreground))',
+          DEFAULT: "hsla(var(--secondary))",
+          foreground: "hsla(var(--secondary-foreground))",
         },
         destructive: {
-          DEFAULT: 'hsla(var(--destructive))',
-          foreground: 'hsla(var(--destructive-foreground))',
+          DEFAULT: "hsla(var(--destructive))",
+          foreground: "hsla(var(--destructive-foreground))",
         },
         muted: {
-          DEFAULT: 'hsla(var(--muted))',
-          foreground: 'hsla(var(--muted-foreground))',
+          DEFAULT: "hsla(var(--muted))",
+          foreground: "hsla(var(--muted-foreground))",
         },
         accent: {
-          DEFAULT: 'hsla(var(--accent))',
-          foreground: 'hsla(var(--accent-foreground))',
+          DEFAULT: "hsla(var(--accent))",
+          foreground: "hsla(var(--accent-foreground))",
         },
         popover: {
-          DEFAULT: 'hsla(var(--popover))',
-          foreground: 'hsla(var(--popover-foreground))',
+          DEFAULT: "hsla(var(--popover))",
+          foreground: "hsla(var(--popover-foreground))",
         },
         card: {
-          DEFAULT: 'hsla(var(--card))',
-          foreground: 'hsla(var(--card-foreground))',
+          DEFAULT: "hsla(var(--card))",
+          foreground: "hsla(var(--card-foreground))",
+        },
+      },
+      borderColor: {
+        outline: {
+          DEFAULT: "hsla(var(--border-grey))",
+          primary: "hsla(var(--border))",
         },
       },
       borderWidth: {
         hairline: hairlineWidth(),
       },
       keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       textColor: {
         primary: {
-          DEFAULT: 'hsla(var(--text-primary))',
-          foreground: 'hsla(var(--text-foreground))',
+          DEFAULT: "hsla(var(--text-primary))",
+          foreground: "hsla(var(--text-foreground))",
         },
         secondary: {
-          DEFAULT: 'hsla(var(--primary))',
-          foreground: 'hsla(var(--primary-foreground))',
+          DEFAULT: "hsla(var(--primary))",
+          foreground: "hsla(var(--primary-foreground))",
         },
-        placeholder: 'hsla(var(--placeholder))'
-      }
+        placeholder: "hsla(var(--placeholder))",
+      },
+      fontFamily: {
+        gotham: ["Gotham-Rounded-Medium"],
+        "gotham-light": ["Gotham-Rounded-Light"],
+        "gotham-bold": ["Gotham-Rounded-Bold"],
+        lato: ["Lato"],
+        "lato-light": ["Lato-Light"],
+        "lato-bold": ["Lato-Bold"],
+      },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require("tailwindcss-animate")],
 };
