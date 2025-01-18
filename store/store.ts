d0f85f4c64e1a44036e8cd4c87cf69f2c4669077
saimoulibandari <saimouli.bandari@gmail.com>
2025-01-18 19:56:47 +0530

    updated store

--- store/store.ts
+++ store/store.ts
@@ -7,9 +7,29 @@ interface IGeoLocationRegion {
     latitudeDelta: number;
 }
 
+interface IAddress {
+    address: string,
+    pincode: string,
+    city: string,
+    state: string,
+    houseFlat: string,
+    buildingNo: string,
+    roadName: string,
+}
+
+interface IUserDetails {
+    receiverName: string,
+    receiverPhone: string,
+    petName: string,
+}
+
 interface IStore {
     location: IGeoLocationRegion | null;
     address: any | null;
+    deliveryAddress: {
+        address: IAddress,
+        user: IUserDetails
+    }
     isLocationPermissionGranted: boolean;
     setLocation: (location: IGeoLocationRegion) => void;
     clearLocation: () => void;
@@ -18,12 +38,29 @@ interface IStore {
     setLocationAndAddress: (location: IGeoLocationRegion, address: any) => void;
     clearLocationAndAddress: () => void;
     setIsLocationPermissionGranted: (isLocationPermissionGranted: boolean) => void;
+    setDeliveryAddress: (address: IAddress, user: IUserDetails) => void
 }
 
 const useStore = create<IStore>((set) => ({
-    location: null,
+    location: null, // rename to region
     address: null,
     isLocationPermissionGranted: false,
+    deliveryAddress: {
+        address: {
+            address: '',
+            pincode: '',
+            city: '',
+            state: '',
+            houseFlat: '',
+            buildingNo: '',
+            roadName: ''
+        },
+        user: {
+            receiverName: '',
+            receiverPhone: '',
+            petName: ''
+        }
+    },
     setLocation: (location: IGeoLocationRegion) => set({ location }),
     setAddress: (address: any) => set({ address }),
     clearLocation: () => set({ location: null }),
@@ -31,6 +68,7 @@ const useStore = create<IStore>((set) => ({
     setLocationAndAddress: (location: IGeoLocationRegion, address: any) => set({ location, address }),
     clearLocationAndAddress: () => set({ location: null, address: null }),
     setIsLocationPermissionGranted: (isLocationPermissionGranted: boolean) => set({ isLocationPermissionGranted }),
+    setDeliveryAddress: (address: IAddress, user: IUserDetails) => set({ deliveryAddress: { address, user } })
 }));
 
 export default useStore;
