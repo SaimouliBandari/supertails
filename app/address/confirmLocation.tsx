d49954eb949422c971ed024e4d9ae5dcbede4540
saimoulibandari <saimouli.bandari@gmail.com>
2025-01-18 19:21:46 +0530

    added confirmation modal.

--- app/address/confirmLocation.tsx
+++ app/address/confirmLocation.tsx
@@ -1,18 +1,43 @@
 import AddressPlaceholder from "@/components/addressPlaceholder";
 import Header from "@/components/header/header";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import useStore from "@/store/store";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import { useLocalSearchParams, useRouter } from "expo-router";
-import React, { useRef } from "react";
+import { Image } from "expo-image";
+import { useRouter } from "expo-router";
+import React, { lazy, useRef } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { KeyboardAvoidingView, Platform, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
-import type { Region } from "react-native-maps";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Image } from "expo-image";
 import { regionDataGenerator } from "./location";
-import useStore from "@/store/store";
-import { generateAddress } from "@/utils/generator.service";
+import classNames from "classnames";
+
+const saveAsButtons = [
+  {
+    key: "home",
+    label: "Home",
+    icon: {
+      active: require("@/assets/images/house-solid-active.svg"),
+      inactive: require("@/assets/images/house-solid.png")
+    }
+  },
+  {
+    key: "office",
+    label: "Office",
+    icon: {
+      active: require("@/assets/images/work-solid-active.svg"),
+      inactive: require("@/assets/images/office-solid.png")
+    }
+  },
+  {
+    key: "others",
+    label: "Others",
+    icon: {
+      active: require("@/assets/images/others-solid.svg"),
+      inactive: require("@/assets/images/others-solid.png")
+    }
+  }
+]
 
 export default function LocationPicker() {
   const mapRef = useRef<MapView>(null);
@@ -31,6 +56,7 @@ export default function LocationPicker() {
       receiverName: "",
       receiverPhone: "",
       petName: "",
+      saveAddressAs: "",
     },
   });
 
@@ -68,19 +94,27 @@ export default function LocationPicker() {
               <MapView
                 ref={mapRef}
                 style={{ flex: 1 }}
-                // initialRegion={{
-                //   latitude: 37.78825,
-                //   longitude: -122.4324,
-                //   latitudeDelta: 0.0922,
-                //   longitudeDelta: 0.0421,
-                // }}
                 region={regionDataGenerator((region as any).latitude, (region as any).longitude)}
+                scrollEnabled={false}
+              >
+                <Marker
 
-              ></MapView>
+                  coordinate={{
+                    latitude: +(region?.latitude || 0),
+                    longitude: +(region?.longitude || 0),
+                  }}
+                >
+                  <View className="flex flex-col justify-center items-center">
+                    <View className="flex flex-row justify-center items-center bg-[#DAFF6F] w-[38px] h-[38px] rounded-full opacity-[50%]">
+                      <View className="bg-[#1F2937] w-[12px] h-[12px] rounded-full"></View>
+                    </View>
+                  </View>
+                </Marker>
+              </MapView>
             </View>
             <View className="bg-card flex flex-1">
               <View className="h-[91px] py-[16px] border-b-[0.4px] border-b-border">
-                <AddressPlaceholder onChange={() => { router.back() }} address={generateAddress(address)} name={address.name} />
+                <AddressPlaceholder onChange={() => { router.back() }} address={address.formatted_address} name={address?.name} />
               </View>
               <ScrollView className="px-[12px]">
                 <View className="mt-[16px]">
@@ -158,35 +192,69 @@ export default function LocationPicker() {
                   <Text className="font-gotham font-[350] text-[16px] leading-[19.2px] mb-[12px]">
                     Save address as
                   </Text>
-                  <View className="flex flex-row gap-2">
-                    <View className="w-[68px] h-[26px] rounded-[4px] border-[0.4px] flex flex-row justify-center items-center border-outline">
-                      <Image
-                        source={require("../../assets/images/house-solid.png")}
-                        style={{ width: 18, height: 18 }}
-                      />
-                      <Text className="font-gotham py-[6px] ps-[2px] font-[325] text-[12px] leading-[14.4px]">
-                        Home
-                      </Text>
-                    </View>
-                    <View className="w-[68px] h-[26px] rounded-[4px] border-[0.4px] flex flex-row justify-center items-center border-outline">
-                      <Image
-                        source={require("../../assets/images/office-solid.png")}
-                        style={{ width: 18, height: 18 }}
-                      />
-                      <Text className="font-gotham py-[6px] ps-[2px] font-[325] text-[12px] leading-[14.4px]">
-                        Office
-                      </Text>
-                    </View>
-                    <View className="w-[68px] h-[26px] rounded-[4px] border-[0.4px] flex flex-row justify-center items-center border-outline">
-                      <Image
-                        source={require("../../assets/images/others-solid.png")}
-                        style={{ width: 18, height: 18 }}
-                      />
-                      <Text className="font-gotham py-[6px] ps-[2px] font-[325] text-[12px] leading-[14.4px]">
-                        Others
-                      </Text>
-                    </View>
-                  </View>
+                  <Controller
+                    control={control}
+                    name="saveAddressAs"
+                    rules={{ required: "This field is required" }}
+                    render={({ field: { onChange, value } }) => (
+                      <View className="flex flex-row gap-2">
+                        {
+                          saveAsButtons.map((btnOpts) => {
+                            if (value == "others" && btnOpts.key !== "others") {
+                              return null;
+                            }
+                            const iconKey = value == btnOpts.key ? 'active' : 'inactive'
+                            return <TouchableOpacity onPress={() => onChange(btnOpts.key)} key={btnOpts.key}>
+                              <View className={classNames("w-[68px] h-[26px] rounded-[4px] border-[0.4px] flex flex-row justify-center items-center border-outline", value == btnOpts.key ? "bg-[#FFEAD8] border-primary" : "")}>
+                                <Image
+                                  source={btnOpts.icon[iconKey]}
+                                  style={{ width: 18, height: 18 }}
+                                />
+                                <Text className={classNames("font-gotham py-[6px] ps-[2px] font-[325] text-[12px] leading-[14.4px]", value == btnOpts.key ? " text-secondary" : "")}>
+                                  {btnOpts.label}
+                                </Text>
+                              </View>
+                            </TouchableOpacity>
+                          })
+                        }
+                        {/* {value !== "others" && <>
+                          <TouchableOpacity onPress={() => onChange("home")}>
+                            <View className={classNames("w-[68px] h-[26px] rounded-[4px] border-[0.4px] flex flex-row justify-center items-center border-outline", value == "home" ? "bg-[#FFEAD8] border-primary" : "")}>
+                              <Image
+                                source={require("../../assets/images/house-solid-active.svg")}
+                                style={{ width: 18, height: 18 }}
+                              />
+                              <Text className={classNames("font-gotham py-[6px] ps-[2px] font-[325] text-[12px] leading-[14.4px]", value == "home" ? " text-secondary" : "")}>
+                                Home
+                              </Text>
+                            </View>
+                          </TouchableOpacity>
+                          <TouchableOpacity onPress={() => onChange("office")}>
+                            <View className={classNames("w-[68px] h-[26px] rounded-[4px] border-[0.4px] flex flex-row justify-center items-center border-outline", value == "office" ? "" : "")}>
+                              <Image
+                                source={require("../../assets/images/office-solid.png")}
+                                style={{ width: 18, height: 18 }}
+                              />
+                              <Text className="font-gotham py-[6px] ps-[2px] font-[325] text-[12px] leading-[14.4px]">
+                                Office
+                              </Text>
+                            </View>
+                          </TouchableOpacity>
+                        </>}
+                        <TouchableOpacity onPress={() => onChange("others")}>
+                          <View className={classNames("w-[68px] h-[26px] rounded-[4px] border-[0.4px] flex flex-row justify-center items-center border-outline", value == "others" ? "" : "")}>
+                            <Image
+                              source={require("../../assets/images/others-solid.png")}
+                              style={{ width: 18, height: 18 }}
+                            />
+                            <Text className="font-gotham py-[6px] ps-[2px] font-[325] text-[12px] leading-[14.4px]">
+                              Others
+                            </Text>
+                          </View>
+                        </TouchableOpacity> */}
+                      </View>
+                    )}
+                  />
                   <View className="mt-[15px]">
                     <View className="border border-gray-300 h-[40px] mb-[8px] rounded-[8px]  flex flex-row items-center">
                       {/* <Ionicons name="home-outline" size={16} color={"black"} /> */}
@@ -264,11 +332,11 @@ export default function LocationPicker() {
                   </TouchableOpacity>
                 </View>
               </ScrollView>
-            </View>
-          </View>
-        </View>
-      </KeyboardAvoidingView>
+            </View >
+          </View >
+        </View >
+      </KeyboardAvoidingView >
 
-    </SafeAreaView>
+    </SafeAreaView >
   );
 }
